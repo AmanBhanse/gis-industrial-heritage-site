9b8055bec62eab73da68b4e1e9c1856d024dae16
Aman Bhanse <amanbhanse22@gmail.com>
2026-06-09 19:44:43 +0200

    feat: Add RouteLayer component for thematic walking tour

---
+++ src/components/RouteLayer.jsx
@@ -0,0 +1,54 @@
+import { Polyline, CircleMarker, Tooltip } from 'react-leaflet'
+
+const ROUTE_STYLE = {
+  color: '#1a5276',
+  weight: 3,
+  opacity: 0.8,
+  dashArray: '8 6',
+  lineJoin: 'round',
+}
+
+const WAYPOINT_STYLE = {
+  radius: 7,
+  color: '#1a5276',
+  fillColor: '#2980b9',
+  fillOpacity: 1,
+  weight: 2,
+}
+
+function RouteLayer({ sites = [], route = [], visible = false }) {
+  if (!visible || route.length < 2) return null
+
+  const siteById = Object.fromEntries(sites.map((s) => [s.id, s]))
+
+  const positions = route
+    .map((id) => siteById[id])
+    .filter(Boolean)
+    .map((site) => [site.lat, site.lng])
+
+  const waypoints = route
+    .map((id) => siteById[id])
+    .filter(Boolean)
+
+  if (positions.length < 2) return null
+
+  return (
+    <>
+      <Polyline positions={positions} pathOptions={ROUTE_STYLE} />
+      {waypoints.map((site, index) => (
+        <CircleMarker
+          key={`route-wp-${site.id}`}
+          center={[site.lat, site.lng]}
+          pathOptions={WAYPOINT_STYLE}
+          radius={WAYPOINT_STYLE.radius}
+        >
+          <Tooltip permanent direction="top" offset={[0, -10]} className="route-label">
+            {index + 1}
+          </Tooltip>
+        </CircleMarker>
+      ))}
+    </>
+  )
+}
+
+export default RouteLayer
